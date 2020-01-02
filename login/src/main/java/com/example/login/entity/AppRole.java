62b9d520bd86b2dde68e8fe2ef783533c8c41ee7
thanglovemusic94 <thanglovemusic94@gmail.com>
2020-01-02 16:27:24 +0700

    add file

---
+++ login/src/main/java/com/example/login/entity/AppRole.java
@@ -0,0 +1,28 @@
+package com.example.login.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Data
+@Entity
+@Table(name = "App_Role", //
+        uniqueConstraints = { //
+                @UniqueConstraint(name = "APP_ROLE_UK", columnNames = "Role_Name")})
+
+public class AppRole {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Role_Id", nullable = false)
+    private Long roleId;
+
+    @Column(name = "Role_Name", length = 30, nullable = false)
+    private String roleName;
+
+}
