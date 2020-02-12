2fcde237e3d8c1ac5aef76c3578cb4ee8f08f5f9
thanglovemusic94 <thanglovemusic94@gmail.com>
2020-02-13 03:12:08 +0700

    add thymeleaf layout

---
+++ demo/src/main/java/com/example/demo/controller/IndexControll.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class IndexControll {
+    final String view = "user/";
+    @GetMapping("")
+    public String homePage() {
+        return view +"index";
+    }
+
+
+    @GetMapping("/contact")
+    public String contactPage() {
+        return "contact";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about";
+    }
+}
